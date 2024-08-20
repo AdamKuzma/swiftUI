d88a1def6ae23d0b62ffdbf0e9ae6d3a9fccc975
adamk <adam@esecure.cc>
2024-08-20 19:28:53 -0400

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Adam Kuzma on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
